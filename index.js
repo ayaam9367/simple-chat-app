36fda98f72f64a7cf1f0fcf9672cf16f88c80ee3
ayaam9367 <ayaam.mit@gmail.com>
2024-12-11 12:47:17 +0530

    added chat event

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ app.get('/', (req, res)=>{
 
 io.on('connection', (socket)=>{
     console.log('A user is connected');
+    socket.on('chat message', (msg)=>{
+        console.log('message: ' + msg);
+    })
     socket.on('disconnect', ()=>{
         console.log('user disconnected')
     })
