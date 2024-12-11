1cb1014d129fc7b3c0792c343f1d9b9d90e6b76c
ayaam9367 <ayaam.mit@gmail.com>
2024-12-11 14:26:10 +0530

    broadcasting the chat event to all the connected clients

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.get('/', (req, res)=>{
 io.on('connection', (socket)=>{
     console.log('A user is connected');
     socket.on('chat message', (msg)=>{
+        io.emit('chat message', msg)
         console.log('message: ' + msg);
     })
     socket.on('disconnect', ()=>{
